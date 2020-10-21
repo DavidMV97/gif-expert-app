23958ad1cafed0f69685c937678b2b9f7f626614
david <gmunoz@seedem.co>
2020-10-21 14:19:41 -0500

    Docs agregado

---
+++ docs/precache-manifest.701e48e82e84bbd44c92b4373d9d22b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb806d8c29bcb9164e5eb0ae1fac4056",
+    "url": "/index.html"
+  },
+  {
+    "revision": "248847ab8d1bf93d0456",
+    "url": "/static/css/main.85c82cdb.chunk.css"
+  },
+  {
+    "revision": "981650a3b6d2d8451b72",
+    "url": "/static/js/2.ae99edc5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ae99edc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "248847ab8d1bf93d0456",
+    "url": "/static/js/main.1f81bdc2.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
